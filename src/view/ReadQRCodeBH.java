06154c1512ee214dfc44ee79f009287749ed10a2
sadnwor <153931895+sadnwor@users.noreply.github.com>
2025-04-02 16:42:56 +0700

    Update

--- src/view/ReadQRCodeBH.java
+++ src/view/ReadQRCodeBH.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
 package view;
-
+//done
 //import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
